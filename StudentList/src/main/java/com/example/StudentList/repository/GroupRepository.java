379f84438eea997fbbda36fac759d0e2b9bca809
mehman <mehmanosmanof@gmail.com>
2023-06-07 00:28:03 +0400

    update

--- StudentList/src/main/java/com/example/StudentList/repository/GroupRepository.java
+++ StudentList/src/main/java/com/example/StudentList/repository/GroupRepository.java
@@ -12,7 +12,11 @@ import java.util.Optional;
 @Repository
 public interface GroupRepository extends JpaRepository<StudentGroup, Long> {
 
+<<<<<<< HEAD
     @EntityGraph(type = EntityGraphType.FETCH, attributePaths = {"students", "students.studentInfo", "students.teachers"})
+=======
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = {"students", "students.studentInfo", "students.teachers"})
+>>>>>>> a10df0b (update)
 //  @EntityGraph(type = EntityGraphType.FETCH, value = "group_graph")
     List<StudentGroup> findAll();
 
