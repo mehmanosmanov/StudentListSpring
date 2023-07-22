19ad76dbff23b03036e2f29e4c9ee55526495c10
Mehman <mehmanosmanof@gmail.com>
2023-07-22 10:52:03 +0400

    update

--- StudentList/src/main/java/com/example/StudentList/exception/GlobalExceptionHandler.java
+++ StudentList/src/main/java/com/example/StudentList/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.StudentList.exception;
 
 import com.example.StudentList.dto.response.ErrorMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,37 +13,31 @@ import java.time.LocalDateTime;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(AgeLimitException.class)
-    ResponseEntity<ErrorMessage> handleAgeLimitException(AgeLimitException ex) {
+    @ExceptionHandler({
+            AgeLimitException.class,
+            NotFoundException.class,
+            StudentAlreadyExistsException.class})
+    ResponseEntity<ErrorMessage> handleException(Exception ex) {
         log.info(ex.getMessage(), ex);
         ErrorMessage errorMessage = new ErrorMessage();
         errorMessage.setDate(LocalDateTime.now());
-        errorMessage.setStatus(ex.getStatus());
-        errorMessage.setErrorCode(ex.getStatus().value());
+        errorMessage.setStatus(getHttpStatus(ex));
+        errorMessage.setErrorCode(errorMessage.getStatus().value());
         errorMessage.setErrorMessage(ex.getMessage());
-        return ResponseEntity.status(ex.getStatus()).body(errorMessage);
+        return ResponseEntity.status(errorMessage.getStatus()).body(errorMessage);
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    ResponseEntity<ErrorMessage> handleNotFoundException(NotFoundException ex) {
-        log.info(ex.getMessage(), ex);
-        ErrorMessage errorMessage = new ErrorMessage();
-        errorMessage.setDate(LocalDateTime.now());
-        errorMessage.setStatus(ex.getStatus());
-        errorMessage.setErrorCode(ex.getStatus().value());
-        errorMessage.setErrorMessage(ex.getMessage());
-        return ResponseEntity.status(ex.getStatus()).body(errorMessage);
-    }
-
-    @ExceptionHandler(StudentAlreadyExistsException.class)
-    ResponseEntity<ErrorMessage> handleStudentAlreadyExistsException(StudentAlreadyExistsException ex) {
-        log.info(ex.getMessage(), ex);
-        ErrorMessage errorMessage = new ErrorMessage();
-        errorMessage.setDate(LocalDateTime.now());
-        errorMessage.setStatus(ex.getStatus());
-        errorMessage.setErrorCode(ex.getStatus().value());
-        errorMessage.setErrorMessage(ex.getMessage());
-        return ResponseEntity.status(ex.getStatus()).body(errorMessage);
+    private HttpStatus getHttpStatus(Exception ex) {
+        if (ex instanceof AgeLimitException) {
+            return HttpStatus.BAD_REQUEST;
+        } else if (ex instanceof NotFoundException) {
+            return HttpStatus.NOT_FOUND;
+        } else if (ex instanceof StudentAlreadyExistsException) {
+            return HttpStatus.CONFLICT;
+        } else {
+            // Handle any other exceptions
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
     }
 
 //
--- StudentList/src/main/java/com/example/StudentList/service/StudentServiceImpl.java
+++ StudentList/src/main/java/com/example/StudentList/service/StudentServiceImpl.java
@@ -35,135 +35,133 @@ import java.util.Optional;
 @Slf4j
 public class StudentServiceImpl implements StudentService {
 
-    private final StudentRepository studentRepository;
-    private final GroupRepository groupRepository;
-    private final TeacherRepository teacherRepository;
-    private final StudentMapper studentMapper;
-    private final TeacherMapper teacherMapper;
+   private final StudentRepository studentRepository;
+   private final GroupRepository groupRepository;
+   private final TeacherRepository teacherRepository;
+   private final StudentMapper studentMapper;
+   private final TeacherMapper teacherMapper;
 
 
-    @Value("${file.directory}")
-    private String path;
+   @Value("${file.directory}")
+   private String path;
 
-    private Path path1;
-    private final FileUtil fileUtil;
-    @PostConstruct
-    public void init() {
-        path1 = Paths.get(path);
-    }
-    private final File root = new File("D:\\JAVA\\project_files\\files");
+   private Path path1;
+   private final FileUtil fileUtil;
 
-    @Override
-    public String saveStudentAllData(StudentRequest request, MultipartFile file) throws IOException {
-        final Student student = studentMapper.dtoToStudent(request);
+   //Solved the problem
+   @PostConstruct
+   public void init() {
+      path1 = Paths.get(path);
+   }
+
+   private final File root = new File("D:\\JAVA\\project_files\\files");
+
+   @Override
+   public String saveStudentAllData(StudentRequest request, MultipartFile file) throws IOException {
+      final Student student = studentMapper.dtoToStudent(request);
 //        Files.copy(file.getInputStream(),
 //                this.path1.resolve(Objects.requireNonNull(file.getOriginalFilename())));
 
-        //checking if the group already has
-        Optional<StudentGroup> studentGroup = groupRepository.findByNameAndNumber(request.getGroupName(), request.getGroupNumber());
-        if (studentGroup.isEmpty()) {
-            //if not create new one
-            student.setStudentGroup(new StudentGroup(request.getGroupName(), request.getGroupNumber()));
-        } else {
-            student.setStudentGroup(studentGroup.get());
-        }
-        Optional<Teacher> teacherOptional = teacherRepository.findByNameAndSurName(request.getTeacherName(), request.getTeacherSurname());
-        if (teacherOptional.isEmpty()) {
-            final Teacher teacher = teacherMapper.dtoToEntity(request);
-            teacher.addStudent(student);
-        } else
-            teacherOptional.get().addStudent(student);
+      //checking if the group already has
+      Optional<StudentGroup> studentGroup = groupRepository.findByNameAndNumber(request.getGroupName(), request.getGroupNumber());
+      if (studentGroup.isEmpty()) {
+         //if not create new one
+         student.setStudentGroup(new StudentGroup(request.getGroupName(), request.getGroupNumber()));
+      } else {
+         student.setStudentGroup(studentGroup.get());
+      }
+      //checking if the teacher already has
+      Optional<Teacher> teacherOptional = teacherRepository.findByNameAndSurName(request.getTeacherName(), request.getTeacherSurname());
+      //if not create new one
+      if (teacherOptional.isEmpty()) {
+         final Teacher teacher = teacherMapper.dtoToEntity(request);
+         teacher.addStudent(student);
+      } else
+         teacherOptional.get().addStudent(student);
 
-        studentRepository.save(student);
+      studentRepository.save(student);
 
-        log.info("New student saved {}", request);
-        return "Student saved successfully";
-    }
+      log.info("New student saved {}", request);
+      return "Student saved successfully";
+   }
 
-    @Override
-    public String saveStudent(StudentRequest request) {
-        log.info("Starting to save a new student");
-        if (request.getAge() < 16) throw new AgeLimitException();
-        studentRepository.save(studentMapper.dtoToStudent(request));
-        log.info("New student saved {}", request);
-        return "Student saved successfully";
-    }
+   @Override
+   public String saveStudent(StudentRequest request) {
+      log.info("Starting to save a new student");
+      if (request.getAge() < 16) throw new AgeLimitException();
+      studentRepository.save(studentMapper.dtoToStudent(request));
+      log.info("New student saved {}", request);
+      return "Student saved successfully";
+   }
 
-    @Override
-    public List<StudentGroupResponse> getAll() {
-        log.info("Start to get all possible students");
-        List<StudentGroup> studentGroups = groupRepository.findAll();
+   @Override
+   public List<StudentGroupResponse> getAll() {
+      log.info("Start to get all possible students");
+      List<StudentGroup> studentGroups = groupRepository.findAll();
 //??????????????????????????????????
 //        System.out.println(studentGroups.get(3).getStudents().stream().toList().get(3).getSurname());
-        List<StudentGroupResponse> responses = new ArrayList<>();
-        for (var group : studentGroups) {
-            StudentGroupResponse groupResponse = new StudentGroupResponse();
-            groupResponse.setName(group.getName());
-            groupResponse.setNumber(group.getNumber());
+      List<StudentGroupResponse> responses = new ArrayList<>();
+      for (var group : studentGroups) {
+         StudentGroupResponse groupResponse = new StudentGroupResponse();
+         groupResponse.setName(group.getName());
+         groupResponse.setNumber(group.getNumber());
 
-            for (var student : group.getStudents()) {
-                StudentResponse studentResponse = studentMapper.studentToDto(student);
-                groupResponse.getStudents().add(studentResponse);
-            }
-            responses.add(groupResponse);
-        }
-//        if (studentResponses.isEmpty()) log.warn("Student list is empty");
-        log.info("All students are fetched");
-        return responses;
-    }
+         for (var student : group.getStudents()) {
+            StudentResponse studentResponse = studentMapper.studentToDto(student);
+            groupResponse.getStudents().add(studentResponse);
+         }
+         responses.add(groupResponse);
+      }
+      log.info("All students are fetched");
+      return responses;
+   }
 
-    @Override
-    public StudentResponse getById(Long id) {
-        log.info("Checking of {} id student", id);
-        return studentMapper.studentToDto(studentRepository.findById(id).
-                orElseThrow(() -> new NotFoundException("Not found student with such id=" + id)));
-//    log.info("{} id student found", id);
-    }
+   @Override
+   public StudentResponse getById(Long id) {
+      log.info("Checking of {} id student", id);
+      return studentMapper.studentToDto(studentRepository.findById(id).
+              orElseThrow(() -> new NotFoundException("Not found student with such id=" + id)));
+   }
 
-    @Override
-    public List<StudentResponse> getByName(String name) {
-        List<StudentResponse> responses = new ArrayList<>();
-        for (Student sstudent : studentRepository.getByName(name)) {
-            responses.add(studentMapper.studentToDto(sstudent));
-        }
-        return responses;
-    }
+   @Override
+   public List<StudentResponse> getByName(String name) {
+      log.info("Get students by name");
+      List<StudentResponse> responses = new ArrayList<>();
+      for (Student sstudent : studentRepository.getByName(name)) {
+         responses.add(studentMapper.studentToDto(sstudent));
+      }
+      return responses;
+   }
 
-    @Override
-    public String update(StudentRequest request, Long id, MultipartFile image) throws IOException {
-        studentRepository.findById(id).orElseThrow(() -> new NotFoundException("Not found student with such id=" + id));
-        log.info("Updating {} id student", id);
-//        oldStudent = studentMapper.dtoToStudent(request);
-
-        Student student = studentMapper.dtoToStudent(request);
-        student.setId(id);
-        studentRepository.save(student);
-//        oldStudent.getStudentInfo().setImage(image.getOriginalFilename());
+   @Override
+   public String update(StudentRequest request, Long id, MultipartFile image) throws IOException {
+      studentRepository.findById(id).orElseThrow(() -> new NotFoundException("Not found student with such id=" + id));
+      log.info("Updating {} id student", id);
+      Student student = studentMapper.dtoToStudent(request);
+      student.setId(id);
 //        Files.copy(image.getInputStream(), this.path1.resolve(Objects.requireNonNull(image.getOriginalFilename())));
-//        studentRepository.save(oldStudent);
-        log.info("{} id student is updated", id);
-        return "Student with id=" + id + " successfully updated ";
-    }
+      studentRepository.save(student);
+      log.info("{} id student is updated", id);
+      return "Student with id=" + id + " successfully updated ";
+   }
 
-    @Override
-    public String deleteById(Long id) {
-        studentRepository.findById(id).orElseThrow(() -> new NotFoundException("Not found student with such id=" + id));
-        log.info("Starting to delete {} id student", id);
-        studentRepository.deleteById(id);
+   @Override
+   public String deleteById(Long id) {
+      studentRepository.findById(id).orElseThrow(() -> new NotFoundException("Not found student with such id=" + id));
+      log.info("Starting to delete {} id student", id);
+      studentRepository.deleteById(id);
 
-        log.info("{} id student is deleted", id);
-        return "Student id=" + id + " is successfully deleted";
-    }
+      log.info("{} id student is deleted", id);
+      return "Student id=" + id + " is successfully deleted";
+   }
 
-    public Resource getStudentImage(Long id) {
-        Student student = studentRepository.findById(id).orElseThrow(() -> new NotFoundException("Not found student with such id=" + id));
-        return getFile(student.getStudentInfo().getImage()).getBody();
-    }
+   public Resource getStudentImage(Long id) {
+      Student student = studentRepository.findById(id).orElseThrow(() -> new NotFoundException("Not found student with such id=" + id));
+      return getFile(student.getStudentInfo().getImage()).getBody();
+   }
 
-    public ResponseEntity<Resource> getFile(String fileName) {
-        Resource resource = fileUtil.load(fileName, this.path1);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"").body(resource);
-    }
-
-
+   public ResponseEntity<Resource> getFile(String fileName) {
+      Resource resource = fileUtil.load(fileName, this.path1);
+      return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"").body(resource);
+   }
 }
